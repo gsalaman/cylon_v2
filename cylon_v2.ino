424123ff0f94c0ad4545d4788ff06de01460eb60
Glenn Salaman <g.salaman@comcast.net>
2020-04-24 10:16:53 -0600

    Development checkpoint
    Issues:  Eye off right doesn't set red pixel on edge.   Keeps moving right until crash.

--- cylon_v2.ino
+++ cylon_v2.ino
@@ -26,6 +26,12 @@
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LED_PIN, NEO_GRB+NEO_KHZ800);
 
+/* The neopixel driver uses a 24 bit value to refer to color, where
+ *  the upper 8 bits are the red value, the middle 8 green, and the lower 8 blue.
+ *  I'm gonna refer to this as a "composite color".
+ */
+
+/* A few helpful color #defines... */
 #define COLOR_RED     0xFF0000
 #define COLOR_GREEN   0x00FF00
 #define COLOR_BLUE    0x0000FF
@@ -37,8 +43,35 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LED_PIN, NEO_GRB+NEO_KHZ
 
 #define MAX_EYE_SIZE (NUMPIXELS - 1)
 
+/* Window concepts:
+ *  The virtual window is a large array of pixels, but we're only going to 
+ *  show the middle 8 pixels...and we'll refer to those as the "real window" 
+ *  pixels. 
+ */
 #define VIRTUAL_WINDOW_SIZE (NUMPIXELS+2*MAX_EYE_SIZE)
 uint32_t virtual_window[VIRTUAL_WINDOW_SIZE];
+
+#define REAL_WINDOW_START_INDEX MAX_EYE_SIZE 
+#define REAL_WINDOW_END_INDEX (REAL_WINDOW_START_INDEX + NUMPIXELS)
+
+/* The cyclon eye parameters */
+int eye_head_pos=REAL_WINDOW_START_INDEX;  // this is the virutal window index
+int eye_size=4;
+
+typedef enum
+{
+  DIR_RIGHT,
+  DIR_LEFT
+} dir_type;
+
+dir_type current_eye_dir=DIR_RIGHT;
+
+uint32_t eye_color=COLOR_RED;
+uint32_t background_color=COLOR_BLUE;
+
+uint32_t display_delay_ms=1000;
+
+
 
 /*================================================================================
  * fillAll
@@ -53,10 +86,21 @@ void fillAll( uint32_t color )
   }
 }
 
+/* Fills the entire virtual window with a given color */
+void virtual_fill(uint32_t color)
+{
+  int i;
+
+  for (i=0; i<VIRTUAL_WINDOW_SIZE; i++)
+  {
+    virtual_window[i] = color;
+  }
+}
+
 /*========================================================================
  * fillGradient
  *
- * This function fills an array with a gradient from start color to end color.
+ * This function fills an array with a gradient from start_color to end_color.
  */
 void fillGradient(uint32_t *strip, int start_index, uint32_t start_color, int end_index, uint32_t end_color)
 {
@@ -76,6 +120,11 @@ void fillGradient(uint32_t *strip, int start_index, uint32_t start_color, int en
 
   int num_steps = end_index - start_index;
 
+  /* A little shifting and masking:
+   *  The blue byte is the bottom 8 bits
+   *  The green byte is the middle 8 bits
+   *  The red byte is the top 8 bits
+   */
   start_red = start_color & 0xFF0000;
   start_red = start_red >> 16;
   start_green = start_color & 0x00FF00;
@@ -92,7 +141,7 @@ void fillGradient(uint32_t *strip, int start_index, uint32_t start_color, int en
   green_step = end_green - start_green;
   blue_step = end_blue - start_blue;
 
-  #if 0
+  #if 0  // Debug serial prints
   Serial.print("Num steps: ");
   Serial.println(num_steps);
   Serial.print("r/g/b step: ");
@@ -106,7 +155,7 @@ void fillGradient(uint32_t *strip, int start_index, uint32_t start_color, int en
   
   for (i = 0; i <= num_steps; i++)
   {
-    #if 0
+    #if 0  // Debug serial prints
     Serial.print("i=");
     Serial.print(i);
     Serial.print(", r:");
@@ -114,7 +163,11 @@ void fillGradient(uint32_t *strip, int start_index, uint32_t start_color, int en
     Serial.print(" b:");
     Serial.println(start_blue + ((i*blue_step)/num_steps));
     #endif
-    
+
+    /* We're gonna build a composite color by "stepping" through the
+     *  individual color bytes from start to end, and building a 24-bit
+     *  composite color value as we go.
+     */
     composite_color = 
       (start_blue + ((i*blue_step)/num_steps));
     composite_color |=
@@ -122,6 +175,7 @@ void fillGradient(uint32_t *strip, int start_index, uint32_t start_color, int en
     composite_color |= 
       (start_red + ((i*red_step)/num_steps)) << 16;
 
+    /* Finally, set the array value to our composite color. */
     strip[start_index + i] = composite_color;
   }
 
@@ -190,12 +244,13 @@ bool buttonPressed( void )
  *
  * See README for description of real vs virtual window.
  */
-#define REAL_WINDOW_START_INDEX MAX_EYE_SIZE 
-#define REAL_WINDOW_END_INDEX (MAX_EYE_SIZE + NUMPIXELS)
 void display_pixels( void )
 {
   int i;
 
+  /* Show the middle pixels of the virtual window (our "real window") on the 
+   *  stick LEDs.
+   */
   for (i = 0; i < NUMPIXELS; i++)
   {
     pixels.setPixelColor(i, virtual_window[i+REAL_WINDOW_START_INDEX]); 
@@ -231,7 +286,116 @@ void setup()
     
 }
 
+/* this function shows the eye, based on the head position, length, 
+ *  and direction
+ */
+void show_eye( void )
+{ 
+   int tail_pos;  // virtual window index of the tail
+
+   /* start with the virtual window filled with the background color */  
+   virtual_fill(background_color);
+
+   /* Draw in the eye, depending on direction */
+   if (current_eye_dir == DIR_RIGHT)
+   {
+     /* if we're going right, the tail is left of the eye...meaning we 
+      *  fillGradient from the tail to the head
+      */
+     tail_pos = eye_head_pos - eye_size;
+     fillGradient(virtual_window, 
+                  tail_pos, background_color, 
+                  eye_head_pos, eye_color);
+
+     /* if the head of the eye is outside our real window, we want to make the
+      *  edge our eye color
+      */
+     if (eye_head_pos > REAL_WINDOW_END_INDEX)
+     {
+       virtual_window[REAL_WINDOW_END_INDEX] = eye_color;
+     }
+   }   // end of "moving right" adjustments
+   else
+   {
+      /* if we're going left, the tail is right of the eye, 
+       *  meaning we fill gradient from the head to tail.
+       */
+      tail_pos = eye_head_pos + eye_size;
+
+      fillGradient(virtual_window, 
+                  eye_head_pos, eye_color, 
+                  tail_pos, background_color);
+
+     /* if the head of the eye is outside our real window, we want to make the
+      *  edge our eye color
+      */
+     if (eye_head_pos < REAL_WINDOW_START_INDEX)
+     {
+       virtual_window[REAL_WINDOW_START_INDEX] = eye_color;
+     }    
+   }  // end of "moving left" adjustments
+
+   /* Now that we've updated our virtual window with the eye, do the 
+    *  actual display
+    */
+   display_pixels(); 
+}
+
+void update_display(void)
+{
+  static uint32_t last_update_time_ms = 0;
+  uint32_t        curr_time_ms;
+
+  curr_time_ms = millis();
+
+  /* if it hasn't been long enough for an update, just return.  */
+  if (last_update_time_ms + display_delay_ms > curr_time_ms)
+  {
+    return;
+  }
+
+  last_update_time_ms = curr_time_ms;
+
+  Serial.print("eye pos: ");
+  Serial.print(eye_head_pos);
+  Serial.print(", dir= ");
+  Serial.println(current_eye_dir);
+  
+  /* do we need to reverse direction? */
+  if ((current_eye_dir == DIR_RIGHT) && 
+      (eye_head_pos == REAL_WINDOW_END_INDEX + eye_size))
+  {
+    current_eye_dir = DIR_LEFT;
+  }
+  else if ((current_eye_dir == DIR_LEFT) &&
+           (eye_head_pos == REAL_WINDOW_START_INDEX - eye_size))
+  {
+    current_eye_dir = DIR_RIGHT;
+  }
+
+  /* move the eye one slot in the desired direction */
+  if (current_eye_dir == DIR_RIGHT)
+  {
+    eye_head_pos++;
+  }
+  else
+  {
+    eye_head_pos--;
+  }
+  
+  /* ...and display the eye on our pixels. */
+  show_eye();
+}
+
 void loop()
 {
-  display_pixels();
+  
+  /* update display */
+  update_display();
+
+  /* update pot */
+
+  /* check button for eye size updates */
+  
+  
 }
